6e441bd5dfffd401b60e982c4b019cc0806fbf3f
Francisco Quevedo Rivera <franciscoquevedo.nttdata@gmail.com>
2024-10-04 23:48:15 -0300

    test

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -1,7 +1,5 @@
-terraform {
-  required_providers {
-    google = {
-      source = "hashicorp/google"
-    }
-  }
+resource "google_storage_bucket" "bucket" {
+  name     = "${var.project_id}-gcf-source"  # Every bucket name must be globally unique
+  location = "US"
+  uniform_bucket_level_access = true
 }
