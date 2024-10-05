fcea8bce5c99af7cb490df02f63fe0a5ab298f84
Francisco Quevedo Rivera <franciscoquevedo.nttdata@gmail.com>
2024-10-05 11:14:46 -0300

    test

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -2,6 +2,11 @@ resource "google_storage_bucket" "function_bucket" {
   name                        = "${var.project_id}-gcf-source"
   location                    = var.region
   uniform_bucket_level_access = true
+  
+	lifecycle {
+    prevent_destroy = true
+    ignore_changes = [name]
+  }
 }
 
 resource "google_storage_bucket_object" "source_code" {
@@ -16,18 +21,18 @@ resource "google_cloudfunctions2_function" "function" {
   description = "A Python Cloud Function deployed with Terraform"
 
   build_config {
-    runtime     = "python311"
-    entry_point = "hello_get"
-    source {
-      storage_source {
-        bucket = google_storage_bucket.function_bucket.name
-        object = "cf_test_wom/main.zip"
-      }
-    }
+	runtime     = "python311"
+	entry_point = "hello_get"
+	source {
+	  storage_source {
+		bucket = google_storage_bucket.function_bucket.name
+		object = "cf_test_wom/main.zip"
+	  }
+	}
   }
 
   service_config {
-    available_memory   = "256M"
-    timeout_seconds    = 60
+	available_memory   = "256M"
+	timeout_seconds    = 60
   }
 }
