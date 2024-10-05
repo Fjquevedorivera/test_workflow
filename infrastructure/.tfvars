cd5a2ebed274684d214cd5c09728be2411aa143c
Francisco Quevedo Rivera <franciscoquevedo.nttdata@gmail.com>
2024-10-05 00:09:31 -0300

    test

--- infrastructure/.tfvars
+++
@@ -1,6 +0,0 @@
-project_id   = "community-viz-looker"
-region       = "us-central1"
-memory_mb    = 128
-timeout      = 60
-bucket_name  = "bucket-community-viz-looker"
-function_name = "cf_test_wom"
--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -1,5 +1,54 @@
-resource "google_storage_bucket" "bucket" {
-  name     = "${var.project_id}-gcf-source"  # Every bucket name must be globally unique
-  location = "US"
+resource "random_id" "default" {
+  byte_length = 8
+}
+
+resource "google_storage_bucket" "default" {
+  name                        = "${random_id.default.hex}-gcf-source" # Cada nombre de depósito debe ser único a nivel global
+  location                    = "US"
   uniform_bucket_level_access = true
+}
+
+data "archive_file" "default" {
+  type        = "zip"
+  output_path = "/tmp/function-source.zip"
+  source_dir  = "helloworld/helloworldHttp"
+}
+resource "google_storage_bucket_object" "object" {
+  name   = "function-source.zip"
+  bucket = google_storage_bucket.default.name
+  source = data.archive_file.default.output_path # Agregar ruta al código fuente de la función comprimida
+}
+
+resource "google_cloudfunctions2_function" "default" {
+  name        = "function-v2"
+  location    = "us-central1"
+  description = "a new function"
+
+  build_config {
+    runtime     = "nodejs20"
+    entry_point = "helloHttp" # Establecer el punto de entrada
+    source {
+      storage_source {
+        bucket = google_storage_bucket.default.name
+        object = google_storage_bucket_object.object.name
+      }
+    }
+  }
+
+  service_config {
+    max_instance_count = 1
+    available_memory   = "256M"
+    timeout_seconds    = 60
+  }
+}
+
+resource "google_cloud_run_service_iam_member" "member" {
+  location = google_cloudfunctions2_function.default.location
+  service  = google_cloudfunctions2_function.default.name
+  role     = "roles/run.invoker"
+  member   = "allUsers"
+}
+
+output "function_uri" {
+  value = google_cloudfunctions2_function.default.service_config[0].uri
 }
