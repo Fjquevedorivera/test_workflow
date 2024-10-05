74770262f7a46c320b8b35b77095115b8d7adecf
Francisco Quevedo Rivera <franciscoquevedo.nttdata@gmail.com>
2024-10-05 00:26:27 -0300

    test

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -1,54 +1,27 @@
-resource "random_id" "default" {
-  byte_length = 8
-}
-
-resource "google_storage_bucket" "default" {
-  name                        = "${random_id.default.hex}-gcf-source" # Cada nombre de depósito debe ser único a nivel global
-  location                    = "US"
+resource "google_storage_bucket" "function_bucket" {
+  name                        = "${var.project_id}-gcf-source"
+  location                    = var.region
   uniform_bucket_level_access = true
 }
 
-data "archive_file" "default" {
-  type        = "zip"
-  output_path = "/tmp/function-source.zip"
-  source_dir  = "helloworld/helloworldHttp"
-}
-resource "google_storage_bucket_object" "object" {
-  name   = "function-source.zip"
-  bucket = google_storage_bucket.default.name
-  source = data.archive_file.default.output_path # Agregar ruta al código fuente de la función comprimida
-}
-
-resource "google_cloudfunctions2_function" "default" {
-  name        = "function-v2"
-  location    = "us-central1"
-  description = "a new function"
+resource "google_cloudfunctions2_function" "function" {
+  name        = "python-function"
+  location    = var.region
+  description = "A Python Cloud Function deployed with Terraform"
 
   build_config {
-    runtime     = "nodejs20"
-    entry_point = "helloHttp" # Establecer el punto de entrada
+    runtime     = "python311"
+    entry_point = "hello_world"
     source {
       storage_source {
-        bucket = google_storage_bucket.default.name
-        object = google_storage_bucket_object.object.name
+        bucket = google_storage_bucket.function_bucket.name
+        object = "function-source.zip"
       }
     }
   }
 
   service_config {
-    max_instance_count = 1
     available_memory   = "256M"
     timeout_seconds    = 60
   }
-}
-
-resource "google_cloud_run_service_iam_member" "member" {
-  location = google_cloudfunctions2_function.default.location
-  service  = google_cloudfunctions2_function.default.name
-  role     = "roles/run.invoker"
-  member   = "allUsers"
-}
-
-output "function_uri" {
-  value = google_cloudfunctions2_function.default.service_config[0].uri
 }
